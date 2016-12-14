a895934712a71d253b9524735b23277de249f3c6
Vladimir Tolmachev <tolmachewladimir@gmail.com>
2016-12-14 19:17:38 +0300

    changes

--- FunnyBees/Game/Components/QueenBeeBehaviour.cs
+++ FunnyBees/Game/Components/QueenBeeBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LibraProgramming.Windows.StateMachine;
 
 namespace FunnyBees.Game.Components
 {
@@ -6,16 +7,44 @@ namespace FunnyBees.Game.Components
     {
         private static readonly TimeSpan Period = TimeSpan.FromSeconds(1.0d);
 
+        private readonly StateMachine<BehaviourStates, BehaviourActions> states;
+        private int count;
         private TimeSpan lastChecked;
 
         public QueenBeeBehaviour()
         {
             lastChecked = TimeSpan.Zero;
+            states = new StateMachine<BehaviourStates, BehaviourActions>(BehaviourStates.Idle);
+
+            states
+                .Configure(BehaviourStates.Idle)
+                .OnEnter(OnIdle)
+                .Permit(BehaviourActions.Update, BehaviourStates.Tick);
+
+            states
+                .Configure(BehaviourStates.Tick)
+                .OnEnter(OnTick)
+                .Permit(BehaviourActions.Update, BehaviourStates.Toe)
+                .Permit(BehaviourActions.Die, BehaviourStates.Dead);
+
+            states
+                .Configure(BehaviourStates.Toe)
+                .OnEnter(OnToe)
+                .Permit(BehaviourActions.Update, BehaviourStates.Tick)
+                .Permit(BehaviourActions.Die, BehaviourStates.Dead);
+
+            states
+                .Configure(BehaviourStates.Dead)
+                .OnEnter(OnDie)
+                .Ignore(BehaviourActions.Update)
+                .Ignore(BehaviourActions.Die);
         }
 
         public override void Update(TimeSpan elapsed)
         {
-            if (TimeSpan.Zero == lastChecked)
+            states.Fire(BehaviourActions.Update);
+
+            /*if (TimeSpan.Zero == lastChecked)
             {
                 lastChecked = elapsed;
             }
@@ -36,12 +65,66 @@ namespace FunnyBees.Game.Components
                 }
                 
                 lastChecked = elapsed;
-            }
+            }*/
         }
 
         public override void Die()
         {
-            throw new NotImplementedException();
+            states.Fire(BehaviourActions.Die);
+        }
+
+        private void OnDie(BehaviourStates obj)
+        {
+        }
+
+        private void OnIdle(BehaviourStates obj)
+        {
+        }
+
+        private void OnTick(BehaviourStates obj)
+        {
+        }
+
+        private void OnToe(BehaviourStates obj)
+        {
+            if (count++ > 100)
+            {
+                var beehive = Container.Beehive;
+
+                foreach (var bee in beehive.GetComponent<BeeManager>().Bees)
+                {
+                    var worker = bee.GetComponent<WorkingBeeBehaviour>(failIfNotExists: false);
+
+                    if (null == worker)
+                    {
+                        continue;
+                    }
+
+                    worker.Search();
+                }
+
+                count = 0;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private enum BehaviourStates
+        {
+            Idle,
+            Tick,
+            Toe,
+            Dead
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private enum BehaviourActions
+        {
+            Update,
+            Die
         }
     }
 }
